6231494b976d8dbbe0575ffc9eac47ae3f27b4e6
kojhyy <82453031+kojhyy@users.noreply.github.com>
2022-05-27 13:02:54 +0200

    Delete update_git.py

--- update_git.py
+++
@@ -1,8 +0,0 @@
-import os
-from time import sleep
-
-path = os.path.dirname(os.path.realpath(__file__))
-sleep(5)
-os.system("git pull")
-sleep(5)
-os.system(f"python3 {os.path.join(path, 'main.py')}")
