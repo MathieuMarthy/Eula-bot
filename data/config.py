c9103d5bbcc86acd245131ce9a26a98a538d1146
kojhyy <l4p0ubelle@gmail.com>
2023-01-26 18:18:55 +0000

    backup vps

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
 import os
 
 path = os.path.dirname(os.path.abspath(__file__))
-token = ""
+token = "OTE0MjI2MzkzNTY1NDk5NDEy.GQRMhU.FWwzV7xEfkLN1zbLeWJGxihmkvCky4U459kIbw"
 prefix = [
     "!",
     "<@914226393565499412> ",
