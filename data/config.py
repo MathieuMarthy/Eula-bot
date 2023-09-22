067aab4e51fe4f2e1f45f3a24e426bdb6795415e
MathieuMarthy <mathieu.marthy@gmail.com>
2023-09-22 21:03:07 +0200

    fix config path

--- data/config.py
+++ data/config.py
@@ -1,8 +1,9 @@
 import os
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+config_path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.dirname(config_path)
 
-with (open(f"{path}/token.txt", "r")) as f:
+with (open(f"{config_path}/token.txt", "r")) as f:
     token = f.read()
 
 prefix = [
@@ -16,6 +17,6 @@ version = "5.0.7"
 owner_id = 236853417681616906
 timezone = "Europe/Paris"
 
-with (open(f"{path}/reddit.txt", "r")) as f:
+with (open(f"{config_path}/reddit.txt", "r")) as f:
     reddit_id = f.readline().replace("\n", "")
     reddit_secret = f.readline().replace("\n", "")
